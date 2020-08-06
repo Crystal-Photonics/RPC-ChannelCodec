f81a0c094ddf6f856ab81d62c8ecd91ab88c08fa
arne krüger <ak@crystal-photonics.com>
2020-08-06 12:39:17 +0200

    renamed part2

--- src/channel_codec/rpc_channel_codec_crc16.c
+++ src/channel_codec/rpc_channel_codec_crc16.c
@@ -5,29 +5,28 @@
  *      Author: ak
  */
 
-#include "channel_codec/crc16.h"
+#include "channel_codec/rpc_channel_codec_crc16.h"
 
-uint16_t crc16_buffer(uint8_t* data_p, uint16_t length){
-	uint8_t x;
+uint16_t crc16_buffer(uint8_t *data_p, uint16_t length) {
+    uint8_t x;
     uint16_t crc = 0xFFFF;
 
-    while (length--){
+    while (length--) {
         x = crc >> 8 ^ *data_p++;
-        x ^= x>>4;
-        crc = (crc << 8) ^ ((uint16_t)(x << 12)) ^ ((uint16_t)(x <<5)) ^ ((uint16_t)x);
+        x ^= x >> 4;
+        crc = (crc << 8) ^ ((uint16_t)(x << 12)) ^ ((uint16_t)(x << 5)) ^ ((uint16_t)x);
     }
     return crc;
 }
 
-void crc16_online(uint8_t data, uint16_t *crc16){
-	uint8_t x;
+void crc16_online(uint8_t data, uint16_t *crc16) {
+    uint8_t x;
 
     x = *crc16 >> 8 ^ data;
-    x ^= x>>4;
-    *crc16 = (*crc16 << 8) ^ ((uint16_t)(x << 12)) ^ ((uint16_t)(x <<5)) ^ ((uint16_t)x);
+    x ^= x >> 4;
+    *crc16 = (*crc16 << 8) ^ ((uint16_t)(x << 12)) ^ ((uint16_t)(x << 5)) ^ ((uint16_t)x);
 }
 
-
-void crc16_online_init(uint16_t *crc16){
+void crc16_online_init(uint16_t *crc16) {
     *crc16 = 0xFFFF;
 }
