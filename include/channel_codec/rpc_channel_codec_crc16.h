4cefb5f6697e9ef72e1e35990aed08b3c4be3569
arne krüger <ak@crystal-photonics.com>
2020-08-06 12:38:43 +0200

    renamed part1

--- include/channel_codec/rpc_channel_codec_crc16.h
+++ include/channel_codec/rpc_channel_codec_crc16.h
@@ -5,13 +5,13 @@
  *      Author: ak
  */
 
-#ifndef CRC16_H_
-#define CRC16_H_
+#ifndef RPC_CRC16_H_
+#define RPC_CRC16_H_
 
 #include <stdint.h>
 
-uint16_t crc16_buffer(uint8_t* data_p, uint16_t length);
+uint16_t crc16_buffer(uint8_t *data_p, uint16_t length);
 void crc16_online(uint8_t data, uint16_t *crc16);
 void crc16_online_init(uint16_t *crc16);
 
-#endif /* CRC16_H_ */
+#endif /* RPC_CRC16_H_ */
