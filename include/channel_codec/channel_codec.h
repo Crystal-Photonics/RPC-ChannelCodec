2cd907de2910beafac8e97bc4b3a54cad1027bba
arne krüger <ak@crystal-photonics.com>
2022-06-30 12:46:46 +0200

    added channel_codec_conf_comport_t RPC_CHANNEL_CODEC_get_last_request_port(); as a function prototype

--- include/channel_codec/channel_codec.h
+++ include/channel_codec/channel_codec.h
@@ -12,14 +12,10 @@
 extern "C" {
 #endif
 
-
-
 #include <stdint.h>
-
 
 #include "channel_codec/channel_codec_types.h"
 #include "rpc_transmission/client/generated_general/RPC_types.h"
-
 
 #define CHANNEL_BLOCKLENGTH 16
 
@@ -31,19 +27,15 @@ typedef RPC_SIZE_RESULT (*RPC_get_answer_length_t)(const void *buffer, size_t si
 typedef void (*RPC_Parser_init_t)(void);
 #endif
 
-
-
-
 bool channel_is_initialized(channel_codec_instance_t *instance);
 void channel_init_instance(channel_codec_instance_t *instance, char *rxBuffer, size_t rxBufferLength, char *txBuffer, size_t txBufferLength);
 void channel_uninit_instance(channel_codec_instance_t *instance);
 
 void channel_start_message_from_RPC(channel_codec_instance_t *instance, size_t size);
 void channel_push_byte_from_RPC(channel_codec_instance_t *instance, unsigned char byte);
-RPC_RESULT channel_commit_from_RPC(channel_codec_instance_t *instance );
+RPC_RESULT channel_commit_from_RPC(channel_codec_instance_t *instance);
 
 void channel_push_byte_to_RPC(channel_codec_instance_t *instance, unsigned char byte);
-
 
 RPC_SIZE_RESULT RPC_CHANNEL_CODEC_get_request_size(channel_codec_instance_t *instance, const void *buffer, size_t size_bytes);
 RPC_SIZE_RESULT RPC_CHANNEL_CODEC_get_answer_length(channel_codec_instance_t *instance, const void *buffer, size_t size_bytes);
@@ -51,6 +43,7 @@ RPC_SIZE_RESULT RPC_CHANNEL_CODEC_get_answer_length(channel_codec_instance_t *in
 void RPC_CHANNEL_CODEC_parse_request(channel_codec_instance_t *instance, const void *buffer, size_t size_bytes);
 void RPC_CHANNEL_CODEC_parse_answer(channel_codec_instance_t *instance, const void *buffer, size_t size_bytes);
 void RPC_CHANNEL_CODEC_parser_init(channel_codec_instance_t *instance);
+channel_codec_conf_comport_t RPC_CHANNEL_CODEC_get_last_request_port();
 
 #ifdef __cplusplus
 }
